1ff890cfff6332fa7ade44ef78939059a25d54f3
hejiaoyan <hejiaoyan168168@163.com>
2018-07-20 11:37:38 +0800

    add a hero-detaiCom

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 // 关键性的元数据位于 @NgModule 装饰器中。最重要的 @NgModule 装饰器位于顶级类 AppModule 上。
-@NgModule({
+@NgModule({,
   declarations: [
     AppComponent,
-    HeroesComponent   // 自动添加
+    HeroesComponent,   // 自动添加
+    HeroDetailComponent,    
   ],
   // 该应用所需外部模块的列表。
   imports: [
